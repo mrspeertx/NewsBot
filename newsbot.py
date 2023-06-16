b60284dec7eed680fa2eb27d30868a416aba7d87
mrspeertx <100307710+mrspeertx@users.noreply.github.com>
2023-06-16 00:06:53 -0500

    Security Update
    --DO NOT USE IN PRODUCTION--
    There is no way to delete a journalist and there is no access control regarding adding one.
    
    This is in line with SQL injection best practices. Also an example JSON config file. I use mine for a few more API keys for other projects. Be sure to either put the JSON file in the same directory as the python file or change the filepath in the python file.

--- newsbot.py
+++ newsbot.py
@@ -9,14 +9,14 @@ import json
 with sqlite3.connect("DFP.db") as con:
     c = con.cursor()
     ##Delete these lines when debugging gets easier
-    # status_code 0 will mean unpublished and 1 will mean published
-    c.execute('''CREATE TABLE IF NOT EXISTS news(date TEXT, 
-                    journalist_id INT REFERENCES journalist, 
-                    story TEXT, language TEXT, status_code INT)''')
     # update_condition is 1 if the journalist is in the process of formatting a story and 0 if they are not
     c.execute('''CREATE TABLE IF NOT EXISTS journalist(id INT PRIMARY KEY, name TEXT, update_condition INT)''')
     c.execute('''CREATE TABLE IF NOT EXISTS editor(id INT PRIMARY KEY, name text)''')
     c.execute('''CREATE TABLE IF NOT EXISTS subscriber(id INT PRIMARY KEY, language TEXT)''')
+    # status_code 0 will mean unpublished and 1 will mean published
+    c.execute('''CREATE TABLE IF NOT EXISTS news(date TEXT, 
+                    journalist_id INT REFERENCES journalist, 
+                    story TEXT, language TEXT, status_code INT)''')
     for table in c.execute("SELECT name FROM sqlite_master WHERE type = 'table'"):
         print("Table", table[0])
     c.close()
@@ -27,9 +27,15 @@ class BotsCommands:
         self.bot = bot
 
     def start(self, message):   # /start command
-        self.bot.send_message(
-            message.chat.id,
-            'Hello user!\nThis is an example bot.')
+         with sqlite3.connect("DFP.db") as con:
+            c = con.cursor()
+            command = "INSERT INTO subscriber VALUES(:id, :language)"
+            data = {"id": message.from_user.id, "language" : message.from_user.language_code}
+            try:
+                con.execute(command, data)
+                self.bot.send_message(message.from_user.id, 'You have been added to the Dallas Free Press messaging list. Thank you for being an important part of independent local journalism!')
+            except:
+                self.bot.send_message(message.from_user.id, "It looks like you have already enrolled.")
 
         
 
@@ -65,15 +71,15 @@ class MessageListener(Listener):  # Event listener must inherit Listener
             print(journalist_inactive)
                 
 
-
-            command = "SELECT id FROM subscriber WHERE language == '" + message.from_user.language_code + "'"
-            unpublished_tuple = c.execute(command)
+            data = {"language": message.from_user.language_code}
+            command = "SELECT id FROM subscriber WHERE language == (:language)"
+            unpublished_tuple = c.execute(command,data)
 
             if '/add_journalist' in message.text:
                 try:
-                    command = "INSERT INTO journalist VALUES (" + str(message.from_user.id) + ", '" + message.from_user.username + "', 0)"
-                    print(command)
-                    c.execute(command)
+                    command = "INSERT INTO journalist VALUES(:id, :username, :update_condition)"
+                    data = {"id": message.from_user.id, "username": message.from_user.username, "update_condition": 0}
+                    c.execute(command, data)                                                            
                 except:
                     self.bot.send_message(message.from_user.id, "You have already been added to the journalist database")
 
@@ -82,13 +88,15 @@ class MessageListener(Listener):  # Event listener must inherit Listener
 
             if '/publish' in message.text and message.from_user.id in journalist_active:
                 c.execute("UPDATE journalist SET update_condition = 0")
-                print('removed user')
-                command = "SELECT story FROM news WHERE journalist_id == " + str(message.from_user.id) + " AND status_code = 0"
-                print(command)
-                c.execute(command)
+                print('removed user from active journalist')
+                data = {"id": message.from_user.id, "status_code": 0}
+                command = "SELECT story FROM news WHERE journalist_id == (:id) AND status_code = (:status_code)"
+                c.execute(command,data)
                 latest = c.fetchone()
                 print(latest[0])
-                subscribers = c.execute("SELECT id FROM subscriber WHERE language == '" + message.from_user.language_code + "'")
+                command = "SELECT id FROM subscriber WHERE language == (:language_code)"
+                data = {"language_code": message.from_user.language_code}
+                subscribers = c.execute(command, data)
                 for id in subscribers:
                     self.bot.send_message(id[0], latest[0])
                 self.bot.send_message(message.from_user.id, "Story has been published.")
@@ -96,31 +104,42 @@ class MessageListener(Listener):  # Event listener must inherit Listener
             if '/publish' not in message.text and message.from_user.id in journalist_active:               
                 self.bot.send_message(message.from_user.id, message.text)
                 self.bot.send_message(message.from_user.id, 'Is this what you would like to broadcast? Reply /publish if yes or send a new message if no.')
-                command = "DELETE FROM news WHERE journalist_id == " + str(message.from_user.id) + " AND status_code == 0"
-                print(command)
-                c.execute(command)
-                command = "INSERT INTO news VALUES ('" + str(datetime.date.today()) + "', " + str(message.from_user.id) + ", '" + message.text + "', '" + message.from_user.language_code + "', 0)"
-                print(command)
-                c.execute(command)
+                command = "DELETE FROM news WHERE journalist_id == (:id) AND status_code == (:status_code)"
+                data = {"id": message.from_user.id, "status_code": 0}
+                c.execute(command,data)
+                command = "INSERT INTO news VALUES(:date, :id, :message, :language, :status_code)"
+                data = {"date": str(datetime.date.today()), 
+                        "id": message.from_user.id, 
+                        "message": message.text, 
+                        "language": message.from_user.language_code,
+                        "status_code": 0}                 
+                c.execute(command, data)
 
             if '/draft' in message.text and message.from_user.id in journalist_inactive:
                 self.bot.send_message(message.from_user.id, 'Send the message you would like broadcast. I will send it back to you as it will be broadcast for final approval.')
-                command = "UPDATE journalist SET update_condition = 1 WHERE id == " + str(message.from_user.id)
-                print(command)
-                con.execute(command)
+                command = "UPDATE journalist SET update_condition = (:update_condition) WHERE id == (:id)"
+                data = {"update_condition": 1, "id": message.from_user.id}
+                con.execute(command,data)
 
             if '/enroll' in message.text.lower():
-                command = "INSERT INTO subscriber VALUES (" + str(message.from_user.id) + ",  '" + str(message.from_user.language_code) + "')"
-                print(command)
-                con.execute(command)
-                self.bot.send_message(message.from_user.id, 'You have been added. Thank you for being an important part of Dallas Free Press!')
+                command = "INSERT INTO subscriber VALUES(:id, :language)"
+                data = {"id": message.from_user.id, "language": message.from_user.language_code}
+                try:
+                    con.execute(command, data)
+                    self.bot.send_message(message.from_user.id, 'You have been added. Thank you for being an important part of Dallas Free Press!')
+                except:
+                    self.bot.send_message(message.from_user.id, "It looks like you have already enrolled.")
 
                 if message.from_user.language_code != 'en':
-                    self.bot.send_message([[[add admin telegram id here]]], "A user has enrolled with language code " + str(message.from_user.language_code))
+                    f = open('config.json')
+                    config = json.load(f)
+                    admin_id = config['admin_id']
+                    self.bot.send_message(admin_id, "A user has enrolled with language code " + str(message.from_user.language_code))
 
             if '/unenroll' in message.text.lower():
-                command = "SELECT id FROM subscriber WHERE id ==" + str(message.from_user.id)
-                c.execute(command)
+                command = "SELECT id FROM subscriber WHERE id == (:id)"
+                data = {"id": message.from_user.id}
+                c.execute(command,data)
                 subscribe_true = c.fetchone()
                 print(subscribe_true)
                 print(message.from_user.id)
@@ -130,9 +149,10 @@ class MessageListener(Listener):  # Event listener must inherit Listener
                     var = False
 
                 if var == True:
-                    command = "DELETE FROM subscriber WHERE id ==" + str(message.from_user.id)
-                    c.execute(command)
-                    self.bot.send_message(message.from_user.id, "You have been unenrolled from the Dallas Free Press messaging list. Simply type /enroll to opt back in!")
+                    command = "DELETE FROM subscriber WHERE id == (:id)"
+                    data = {"id": message.from_user.id}
+                    c.execute(command,data)
+                    self.bot.send_message(message.from_user.id, "You have been unenrolled from the Dallas Free Press messaging list. \nSimply type /enroll to opt back in!")
                 else:
                     self.bot.send_message(message.from_user.id, 'We could not find you in the Dallas Free Press database. Type /enroll if you are looking to opt in!')
 
